2db8790ac9be0dc87b1ba51ee8f0eb4c80e9d22d
SasminiAlawathugoda <sasminianu@gmail.com>
2019-06-23 00:49:59 +0530

    StudentController, StudentRouter, StudentModel

--- models/StudentModel.js
+++ models/StudentModel.js
@@ -1,101 +1,39 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const StudentEnrollmentSchema = new Schema({
-    studentID: {
-        type: String,
-        require: true
-    },
-
-    courses: [{ type: Schema.Types.ObjectId, ref: 'CourseModel' }],
-    user: [{type: Schema.Types.ObjectId, ref: 'UserModel'}]
-
-});
-
-// const StudentAssignmentsSchema = new Schema({
-//     courseID: {
-//         type: String,
-//         require: true
-//     },
-//     courseName: {
-//         type: String,
-//         require: true
-//     },
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 //
-//     assignmentTitle: {
-//         type: String,
-//         require: true
-//     },
-//     dueDate: {
-//         type:Date,
-//         require: true
-//     }
-//
-// });
-//
-// const StudentExamsSchema = new Schema({
-//     courseID: {
-//         type: String,
-//         require: true
-//     },
-//     courseName: {
-//         type: String,
-//         require: true
-//     },
-//     examTitle: {
-//         type: String,
-//         require: true
-//     },
-//     examDate: {
-//         type:Date,
-//         require: true
-//     },
-//     examDuration: {
-//         type: String,
-//         require: true
-//     }
-//
-//
-// });
-//
-// const StudentCoursesSchema = new Schema({
-//     courseID: {
-//         type: String,
-//         require: true
-//     },
-//     courseName: {
-//         type: String,
-//         require: true
-//     }
-// });
-//
-// const StudentAssignmentUploadSchema = new Schema({
+// const StudentEnrollmentSchema = new Schema({
 //     studentID: {
 //         type: String,
 //         require: true
 //     },
-//     courseID: {
-//         type: String,
-//         require: true
-//     },
-//     files: {
-//         type: String,
-//         require: true
-//     }
+//
+//     courses: [{ type: Schema.Types.ObjectId, ref: 'CourseModel' }],
+//     user: [{type: Schema.Types.ObjectId, ref: 'UserModel'}]
+//
 // });
+//
+//
+// mongoose.model('StudentEnrollment', StudentEnrollmentSchema);
+//
+//
+// module.exports = mongoose;
 
-mongoose.model('StudentEnrollment', StudentEnrollmentSchema);
-// mongoose.model('StudentAssignments', StudentAssignmentsSchema);
-// mongoose.model('StudentExams', StudentExamsSchema);
-// mongoose.model('StudentCourses', StudentCoursesSchema);
-// mongoose.model('StudentAssignmentUpload', StudentAssignmentUploadSchema);
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// mongoose.connect('mongodb://localhost:27017/comments', (err) => {
-//     if (err) {
-//         console.log(err);
-//         process.exit(-1);
-//     }
-//     console.log('Connected to the DB');
-// });
+//StudentModel Schema
+const StudentSchema = Schema({
+
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'UserModel'
+    },
+    courses: [{
+        type: Schema.Types.ObjectId,
+        ref: 'CourseModel'
+    }]
+});
+
+mongoose.model('LecturerModel', StudentSchema);
 
 module.exports = mongoose;
--- routes/routes.js
+++ routes/routes.js
@@ -9,12 +9,13 @@ const notice = require('./noticeRouter');
 const assignment = require('./assignmentRouter');
 const exam = require('./examRouter');
 const course = require('./CourseRouter');
+const student = require('./StudentRouter');
 
-const studentEnrollment = require('./StudentEnrollmentRouter');
-const studentAssignment = require('./StudentAssignmentRouter');
-const studentExam = require('./StudentExamRouter');
-const studentCourses = require('./StudentCoursesRouter');
-const studentAssignmentUpload = require('./StudentAssignmentUploadRouter');
+// const studentEnrollment = require('./StudentEnrollmentRouter');
+// const studentAssignment = require('./StudentAssignmentRouter');
+// const studentExam = require('./StudentExamRouter');
+// const studentCourses = require('./StudentCoursesRouter');
+// const studentAssignmentUpload = require('./StudentAssignmentUploadRouter');
 
 //Routes
 routes.use('/', index);
@@ -23,12 +24,13 @@ routes.use('/notice',notice);
 routes.use('/assignments', assignment);
 routes.use('/exams', exam);
 routes.use('/courses', course);
+routes.use('/students', student);
 
-routes.use('/StudentEnrollmentRouter', studentEnrollment);
-routes.use('/StudentAssignmentRouter', studentAssignment);
-routes.use('/StudentExamRouter', studentExam);
-routes.use('/StudentCoursesRouter', studentCourses);
-routes.use('/StudentAssignmentUploadRouter', studentAssignmentUpload);
+// routes.use('/StudentEnrollmentRouter', studentEnrollment);
+// routes.use('/StudentAssignmentRouter', studentAssignment);
+// routes.use('/StudentExamRouter', studentExam);
+// routes.use('/StudentCoursesRouter', studentCourses);
+// routes.use('/StudentAssignmentUploadRouter', studentAssignmentUpload);
 
 
 
